d76f1557142a8a7092194eaba402457b385c7aaa
erenije <scarecrovv228@yandex.ru>
2019-02-27 15:28:46 +0900

    1231

--- Project1/Project1/numbers.h
+++ Project1/Project1/numbers.h
@@ -73,4 +73,124 @@ string string_from_int(int number)
 
 
 }
+string  number_less_100(int n)
+	{
+	string an;
+	if(n/10==2){
+		int a=n%10;
+		if(a==0) an = "двадцать";
+		else an="двадцать " + string_from_int(a);
+	
+	}
+	else if(n/10==3){
+		int a=n%10;
+		if(a==0) an = "тридцать";
+		else an="тридцать " + string_from_int(a);
+	
+	}
+	else if(n/10==4){
+		int a=n%10;
+		if(a==0) an = "сорок";
+		else an="сорок " + string_from_int(a);
+	
+	}
+	else if(n/10==5){
+		int a=n%10;
+		if(a==0) an = "пятьдесять";
+		else an="пятьдесять " + string_from_int(a);
 
+	}
+	else if(n/10==6){
+		int a=n%10;
+		if(a==0) an = "шестьдесять";
+		else an="шестьдесять " + string_from_int(a);
+
+	}
+	else if(n/10==7){
+		int a=n%10;
+		if(a==0) an = "семьдесять";
+		else an="семьдесять " + string_from_int(a);
+
+	}
+	else if(n/10==8){
+		int a=n%10;
+		if(a==0) an = "восемьдесять";
+		else an="восемьдесять " + string_from_int(a);
+
+	}
+	else if(n/10==9){
+		int a=n%10;
+		if(a==0) an = "девяносто";
+		else an="девяносто " + string_from_int(a);
+
+	}
+
+		
+		return an;
+}
+string  number_less_1000(int num)
+	{
+	string ans;
+	if(num/100==1){
+		int a=num%100;
+		if(a==0) ans = "сто";
+		else ans="сто " + string_from_int(a);
+			}
+	else if(num/100==2){
+		int a=num%100;
+		if(a==0) ans = "двести";
+		else ans="двести " + string_from_int(a);
+
+	}
+	else if(num/100==3){
+		int a=num%100;
+		if(a==0) ans = "триста";
+		else ans="триста " + string_from_int(a);
+
+	}
+	else if(num/100==4){
+		int a=num%100;
+		if(a==0) ans = "четыреста";
+		else ans="четыреста " + string_from_int(a);
+
+	}
+	else if(num/100==5){
+		int a=num%100;
+		if(a==0) ans = "пятьсот";
+		else ans="пятьсот " + string_from_int(a);
+
+	}
+	else if(num/100==6){
+		int a=num%100;
+		if(a==0) ans = "шестьсот";
+		else ans="шестьсот " + string_from_int(a);
+
+	}
+	else if(num/100==7){
+		int a=num%100;
+		if(a==0) ans = "семьсот";
+		else ans="семьсот " + string_from_int(a);
+
+	}
+	else if(num/100==8){
+		int a=num%100;
+		if(a==0) ans = "восемьсот";
+		else ans="восемьсот " + string_from_int(a);
+
+	}
+	else if(num/100==9){
+		int a=num%100;
+		if(a==0) ans = "девятьсот";
+		else ans="девятьсот " + string_from_int(a);
+
+	}
+	else if(num/100==10){
+		int a=num%100;
+		if(a==0) ans = "тысяча";
+		else ans="тысяча " + string_from_int(a);
+
+	}
+
+
+	return ans;
+}
--- Project1/Project1/tests.cpp
+++ Project1/Project1/tests.cpp
@@ -4,34 +4,67 @@
 #include <boost/test/included/unit_test.hpp>
 #include "numbers.h"
 
+
 BOOST_AUTO_TEST_SUITE( test_suite1 )
 
 
- BOOST_AUTO_TEST_CASE( test_case1 )
+// BOOST_AUTO_TEST_CASE( test_case1 )
+//{
+//	
+////BOOST_TEST(string_from_int(0)==string("ноль"));
+////BOOST_TEST(string_from_int(1)==string("один"));
+////BOOST_TEST(string_from_int(2)==string("два"));
+////BOOST_TEST(string_from_int(3)==string("три"));
+////BOOST_TEST(string_from_int(4)==string("четыре"));
+////BOOST_TEST(string_from_int(5)==string("пять"));
+////BOOST_TEST(string_from_int(6)==string("шесть"));
+////BOOST_TEST(string_from_int(7)==string("семь"));
+////BOOST_TEST(string_from_int(8)==string("восемь"));
+////BOOST_TEST(string_from_int(9)==string("девять"));
+////BOOST_TEST(string_from_int(10)==string("десять"));
+////BOOST_TEST(string_from_int(11)==string("одинадцать"));
+////BOOST_TEST(string_from_int(12)==string("двенадцать"));
+////BOOST_TEST(string_from_int(13)==string("тринадцать"));
+////BOOST_TEST(string_from_int(14)==string("четырнадцать"));
+////BOOST_TEST(string_from_int(15)==string("пятьнадцать"));
+////BOOST_TEST(string_from_int(16)==string("шестьнадцать"));
+////BOOST_TEST(string_from_int(17)==string("семьнадцать"));
+////BOOST_TEST(string_from_int(18)==string("восемьнадцать"));
+////BOOST_TEST(string_from_int(19)==string("девятьнадцать"));
+//
+
+//}
+//BOOST_AUTO_TEST_CASE( test_case2 )
+//{
+//	
+//BOOST_TEST(number_less_100(20)==string("двадцать"));
+//BOOST_TEST(number_less_100(35)==string("тридцать пять"));
+//BOOST_TEST(number_less_100(46)==string("сорок шесть"));
+//BOOST_TEST(number_less_100(56)==string("пятьдесять шесть"));
+//BOOST_TEST(number_less_100(62)==string("шестьдесять два"));
+//BOOST_TEST(number_less_100(77)==string("семьдесять семь"));
+//BOOST_TEST(number_less_100(87)==string("восемьдесять семь"));
+//BOOST_TEST(number_less_100(91)==string("девяносто один"));
+//
+//
+//}
+BOOST_AUTO_TEST_CASE( test_case3 )
 {
 	
-BOOST_TEST(string_from_int(0)==string("ноль"));
-BOOST_TEST(string_from_int(1)==string("один"));
-BOOST_TEST(string_from_int(2)==string("два"));
-BOOST_TEST(string_from_int(3)==string("три"));
-BOOST_TEST(string_from_int(4)==string("четыре"));
-BOOST_TEST(string_from_int(5)==string("пять"));
-BOOST_TEST(string_from_int(6)==string("шесть"));
-BOOST_TEST(string_from_int(7)==string("семь"));
-BOOST_TEST(string_from_int(8)==string("восемь"));
-BOOST_TEST(string_from_int(9)==string("девять"));
-BOOST_TEST(string_from_int(10)==string("десять"));
-BOOST_TEST(string_from_int(11)==string("одинадцать"));
-BOOST_TEST(string_from_int(12)==string("двенадцать"));
-BOOST_TEST(string_from_int(13)==string("тринадцать"));
-BOOST_TEST(string_from_int(14)==string("четырнадцать"));
-BOOST_TEST(string_from_int(15)==string("пятьнадцать"));
-BOOST_TEST(string_from_int(16)==string("шестьнадцать"));
-BOOST_TEST(string_from_int(17)==string("семьнадцать"));
-BOOST_TEST(string_from_int(18)==string("восемьнадцать"));
-BOOST_TEST(string_from_int(19)==string("девятьнадцать"));
+BOOST_TEST(number_less_1000(100)==string("сто"));
+BOOST_TEST(number_less_1000(200)==string("двести"));
+BOOST_TEST(number_less_1000(300)==string("триста"));
+BOOST_TEST(number_less_1000(400)==string("четыреста"));
+BOOST_TEST(number_less_1000(505)==string("пятьсот пять"));
+BOOST_TEST(number_less_1000(600)==string("шестьсот"));
+BOOST_TEST(number_less_1000(700)==string("семьсот"));
+BOOST_TEST(number_less_1000(800)==string("восемьсот"));
+BOOST_TEST(number_less_1000(900)==string("девятьсот"));
+BOOST_TEST(number_less_1000(1000)==string("тысяча"));
 
 
 }
+
+
 BOOST_AUTO_TEST_SUITE_END()
 #endif
